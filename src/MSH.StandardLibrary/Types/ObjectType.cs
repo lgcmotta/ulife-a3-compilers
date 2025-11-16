ff0abbe8767cf7929699c822d9f0e6354c44fe99
lgcmotta <lgcmotta01@gmail.com>
2025-11-16 14:21:42 -0300

    chore: add test projects and fix dependencies with vulnerabilities

--- src/MSH.StandardLibrary/Types/ObjectType.cs
+++ src/MSH.StandardLibrary/Types/ObjectType.cs
@@ -11,7 +11,7 @@ public sealed class ObjectType : Variant<ObjectType>, IVariant
 
     public object Value { get; }
 
-    public static IVariant Null => new ObjectType(null);
+    public static IVariant Null => new ObjectType(new object());
 
     public static bool operator ==(ObjectType? left, ObjectType? right)
     {
---
+++ tests/MSH.StandardLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MSH.StandardLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
